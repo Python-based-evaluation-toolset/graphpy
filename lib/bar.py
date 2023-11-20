1c06353448b1a2c36507b393480078c70c75b720
larycoder <lenhuchuhiep99@gmail.com>
2023-11-21 02:16:51 +0700

    graphpy: [fix] render bar by legend instead of group or stack

--- lib/bar.py
+++ lib/bar.py
@@ -3,29 +3,35 @@ from .utils import plt_save_close
 import matplotlib.pyplot as plt
 
 
-def bar_build(table, output):
+def bar_build(table, output, namespace: str = None):
     fix, ax = plt.subplots()
-    legend_idx = 0
+    legends = table.legend_get(namespace)
+    pre_legend = None
+    bottom = [0 for i in range(len(table.obj_get()))]
+    group = 0
+    stack = 1
     # build bar
-    for group in range(table.group):
+    for legend in legends:
         ax.bar(
             table.obj_arange_get(group),
-            table.data_get_by_idx(legend_idx),
+            table.data_get(legend),
             width=table.width,
+            bottom=bottom,
         )
-        legend_idx += 1
-        for i in range(table.stack - 1):
-            ax.bar(
-                table.obj_arange_get(group),
-                table.data_get_by_idx(legend_idx),
-                width=table.width,
-                bottom=table.data_get_by_idx(legend_idx - 1),
-            )
-            legend_idx += 1
+        if stack < table.stack:
+            stack += 1
+            data = table.data_get(legend)
+            for i in range(len(bottom)):
+                bottom[i] += data[i]
+        elif group < table.group:
+            stack = 1
+            group += 1
+            bottom = [0 for i in bottom]
+
     # label
     ax.set_title(table.title)
     ax.set_xticks(table.obj_arange_get(), table.obj_get())
     ax.set_ylabel(table.ytitle)
     ax.set_xlabel(table.xtitle)
-    ax.legend(table.legend_get()[0:legend_idx])
+    ax.legend(legends)
     plt_save_close(ax, output)
--- test.py
+++ test.py
@@ -9,11 +9,13 @@ if __name__ == "__main__":
         "stack",
         "g1-output",
         "g1-stack",
+        "g2-output",
+        "g2-stack",
     ]
     obj = {
-        "bar_0": [1, 2, 4, 2],
-        "bar_1": [2, 2, 1, 5],
-        "bar_2": [3, 2, 7, 1],
+        "bar_0": [1, 2, 4, 2, 5, 2],
+        "bar_1": [2, 2, 1, 5, 1, 3],
+        "bar_2": [3, 2, 7, 1, 2, 4],
     }
     table = Table(legend)
     table.title_set("interface demonstration")
@@ -22,6 +24,6 @@ if __name__ == "__main__":
     for key, value in obj.items():
         table.object_add(key, value)
     table.stack = 2
-    table.group = 2
+    table.group = 3
     bar_build(table, "demo-bar.pdf")
     plot_build(table, "demo-plot.pdf")
